924f4fee2d9ff1f0eb28f0aea267c5ca5284204d
Zeus <masgar001@gmail.com>
2016-10-24 00:54:56 -0700

    Initial Commit

---
+++ Task/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Task
+//
+//  Created by IT on 10/24/16.
+//  Copyright © 2016 z0s. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
